98ca8ec0a0b9e457fe4e7713ed6d382201e6006c
Allan Hillyer <athillyer@profben.com>
2026-09-02 11:53:44 -0700

    baseline

---
+++ Makefile
@@ -0,0 +1,17 @@
+TARGET = bin/tfFund
+SRC = $(wildcard src/*.c)
+OBJ = $(patsubst src/%.c, obj/%.o, $(SRC))
+
+run: clean default
+
+default: $(TARGET)
+
+clean:
+	rm -f obj/*.o
+	rm -f bin/*
+
+$(TARGET): $(OBJ)
+	gcc -o $@ $?
+
+obj/%.o : src/%.c
+	gcc -c $< -o $@ -Iinc
---
+++ src/threadpool.c
@@ -0,0 +1,29 @@
+#include <threadpool.h>
+#include <stdio.h>
+
+void threadpool_init(threadpool_t* pool)
+{
+    pool->queued = 0;
+    pool->queue_front = 0;
+    pool->queue_back = 0;
+    pool->stop = 0;
+
+    pthread_mutex_init (&(pool->lock), NULL); //one mutex for all threads
+    pthread_cond_init (&(pool->notify), NULL); //one notifier
+
+    for(int i = 0; i < THREADS; i++)
+    {
+        pthread_create(&(pool->threads[i]), NULL, (void *)example_task, pool);
+    }
+}
+
+void example_task(void* arg)
+{
+    pthread_t self = pthread_self();
+    printf("Hello from thread %d", (int)self);
+}
+
+void threadpool_add_task(threadpool_t* pool, void (*function)(void*), void* arg)
+{
+
+}
